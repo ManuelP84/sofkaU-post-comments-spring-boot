f375471cce22f8db656a1a76f08ecb3823ebb3ef
Manuel Pineda <pineda.manuel@gmail.com>
2022-05-04 10:47:36 -0500

    Project initiation

---
+++ src/main/java/com/sofkaU/postcommentsapi/PostCommentsApiApplication.java
@@ -0,0 +1,13 @@
+package com.sofkaU.postcommentsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostCommentsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostCommentsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sofkaU/postcommentsapi/PostCommentsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofkaU.postcommentsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostCommentsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
