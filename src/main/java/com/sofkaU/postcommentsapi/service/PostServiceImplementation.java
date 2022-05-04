7bb1240d8369cb72bd80d8de81fb109bc74d57ed
Manuel Pineda <pineda.manuel@gmail.com>
2022-05-04 15:36:31 -0500

    Service fix ---- Fix @Service anotation. From PostService to PostServiceImplementation

--- src/main/java/com/sofkaU/postcommentsapi/service/PostServiceImplementation.java
+++ src/main/java/com/sofkaU/postcommentsapi/service/PostServiceImplementation.java
@@ -5,9 +5,11 @@ import com.sofkaU.postcommentsapi.entity.Post;
 import com.sofkaU.postcommentsapi.repository.CommentRepository;
 import com.sofkaU.postcommentsapi.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class PostServiceImplementation implements PostService{
 
     @Autowired
@@ -23,7 +25,7 @@ public class PostServiceImplementation implements PostService{
 
     @Override
     public Post createComment(Comment comment) {
-        Post post = postRepository.findById(comment.getFK_post_id()).get();
+        Post post = postRepository.findById(comment.getFkPostId()).get();
         post.addComment(comment);
         commentRepository.save(comment);
         return postRepository.save(post);
